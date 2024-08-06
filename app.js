4aa65257c8b29435e23e627e3e830eeaf7213aeb
Akshay Bhinge <87247305+akshaybhinge19@users.noreply.github.com>
2024-08-06 12:22:44 +0530

    babel, jasx and react element

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
 // React.createElement => React Element(Object) => html Element (render)
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const header = React.createElement('h1',{id: 'header'},'Great to see you!');
-
-console.log(header);
+// const header = React.createElement('h1',{id: 'header'},'Great to see you!');
 
 //JSX is not html in JS
 //JSX is HTML-like Syntax
 //JSX => transpiled before rendering => PARCEL -> Babel
 //JSX => Babel transpile it to React.createElement => React Element(Object) => html Element (render)
-const jsxHeader = <h1 className='header' tabIndex={1}>Namaste React using JSX 🚀</h1>
+// const jsxHeader = <h1 className='header' tabIndex={1}>Namaste React using JSX 🚀</h1>
+const content = (<div id="main-container">
+    <h1 id="heading">
+        I am Babel 
+      </h1>
+    <p>
+      My Role is to transforms JSX into regular JavaScript function calls that create React elements
+    </p>
+    <p>
+    I (Babel) transpiles JSX to React.createElement- React Element(Object) -- html Element (render)
+    </p>
+  </div>)
 
-console.log(jsxHeader);
-root.render(jsxHeader);
+console.log(content);
+root.render(content);
